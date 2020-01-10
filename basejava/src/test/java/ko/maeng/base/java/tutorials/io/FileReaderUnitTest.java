a0a3c779a76b33b5ca3e5bd3536de1e3a2074447
Rebwon <msolo021015@naver.com>
2020-01-10 12:05:11 +0900

    IO 공부중..

---
+++ basejava/src/test/java/ko/maeng/base/java/tutorials/io/FileReaderUnitTest.java
@@ -0,0 +1,18 @@
+package ko.maeng.base.java.tutorials.io;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class FileReaderUnitTest {
+    protected static final String TEXT_FILENAME = "src/test/resources/sampleTextFile.txt";
+
+    @Test
+    public void whenParsingExistingTextFile_thenGetArrayList() throws IOException {
+        List<String> lines = FileReaderExample.generateArrayListFromFile(TEXT_FILENAME);
+        assertTrue("File does not has 2 lines", lines.size() == 2);
+    }
+}
---
+++ basejava/src/test/java/ko/maeng/base/java/tutorials/io/ScannerIntUnitTest.java
@@ -0,0 +1,18 @@
+package ko.maeng.base.java.tutorials.io;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ScannerIntUnitTest {
+    protected static final String NUMBER_FILENAME = "src/test/resources/sampleNumberFile.txt";
+
+    @Test
+    public void whenParsingExistingTextFile_thenGetIntArrayList() throws IOException {
+        List<Integer> numbers = ScannerIntExample.generateArrayListFromFile(NUMBER_FILENAME);
+        assertTrue("File does not has 2 lines", numbers.size() == 2);
+    }
+}
