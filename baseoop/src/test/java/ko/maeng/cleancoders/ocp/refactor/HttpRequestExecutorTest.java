cde582193e6a405457626729539ac8f11c6fde43
Rebwon <msolo021015@naver.com>
2020-06-03 16:19:29 +0900

    ocp httpResponse step2

--- baseoop/src/test/java/ko/maeng/cleancoders/ocp/refactor/HttpRequestExecutorTest.java
+++ baseoop/src/test/java/ko/maeng/cleancoders/ocp/refactor/HttpRequestExecutorTest.java
@@ -5,16 +5,30 @@ import static org.assertj.core.api.Assertions.*;
 import java.io.IOException;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class HttpRequestExecutorTest {
 	private String requestURI = "https://www.daum.net/";
 	private Map<String,String> params;
+	private Boolean isGet = true;
+	private HttpRequestExecutor executor;
+
+	@BeforeEach
+	void setUp() {
+		executor = new HttpRequestExecutor();
+	}
 
 	@Test
-	void foo() throws IOException {
-		HttpRequestExecutor executor = new HttpRequestExecutor();
-		ResponseModel responseModel = executor.handleRequest(requestURI, params);
+	void get() throws IOException {
+		ResponseModel responseModel = executor.handleRequest(isGet, requestURI, params);
+		assertThat(responseModel.getResponseAsText()).startsWith("<!DOCTYPE html><html lang=\"ko\" class=\"\"><head><meta charset=\"utf-8\"/><title>Daum</title>");
+	}
+
+	@Test
+	void post() throws IOException {
+		isGet = false;
+		ResponseModel responseModel = executor.handleRequest(isGet, requestURI, params);
 		assertThat(responseModel.getResponseAsText()).startsWith("<!DOCTYPE html><html lang=\"ko\" class=\"\"><head><meta charset=\"utf-8\"/><title>Daum</title>");
 	}
 }
