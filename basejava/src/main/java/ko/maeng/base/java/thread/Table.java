58f12d1f6126d740abfc9452af45dd11875ea604
Rebwon <msolo021015@naver.com>
2020-06-18 11:07:36 +0900

    producer consumer step2

--- basejava/src/main/java/ko/maeng/base/java/thread/Table.java
+++ basejava/src/main/java/ko/maeng/base/java/thread/Table.java
@@ -1,36 +1,22 @@
 package ko.maeng.base.java.thread;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Table {
-	private final String[] buffer;
-	private int tail;
-	private int head;
-	private int count;
+	private final BlockingQueue<String> buffer;
 
 	public Table(int count) {
-		this.buffer = new String[count];
-		this.head = 0;
-		this.tail = 0;
-		this.count = 0;
+		this.buffer = new ArrayBlockingQueue<String>(10);
 	}
 
-	public synchronized void put(String packet) throws InterruptedException {
-		while(count >= buffer.length) {	// 버퍼가 차면 대기
-			wait();
-		}
-		buffer[tail] = packet;	// 후입
-		tail = (tail+1) % buffer.length;	// Circular 큐라서 tail 위치 바뀜
-		count++;
-		notifyAll();	// 버퍼에 먼가가 들어갔으니 take 해도 된다고 이벤트 날림
+	public void put(String packet) throws InterruptedException {
+		Thread.sleep(1000);
+		buffer.put(packet);
 	}
 
-	public synchronized String take() throws InterruptedException {
-		while(count <= 0) {		// 버퍼에 아무것도 없으면 대기
-			wait();
-		}
-		String packet = buffer[head];	// 선출
-		head = (head+1) % buffer.length; // Circular 큐라서 head 위치 바뀜
-		count--;
-		notifyAll();	// 버퍼에서 하나를 가져갔으니 put해도 된다고 이벤트 날림
+	public String take() throws InterruptedException {
+		String packet = buffer.take();
 		return packet;
 	}
 }
