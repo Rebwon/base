84216ed3bf4c4bff7c5f6a3d0edbe43b8e623682
Rebwon <msolo021015@naver.com>
2020-12-18 21:11:02 +0900

    generic quiz solve

--- basejava/src/main/java/ko/maeng/base/java/generic/Generic.java
+++ basejava/src/main/java/ko/maeng/base/java/generic/Generic.java
@@ -1,10 +1,32 @@
 package ko.maeng.base.java.generic;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 public class Generic<T> {
 
+  protected Class<T> classT;
   protected T objectT;
 
+  @SuppressWarnings("unchecked")
   public Generic() {
-    // Todo T 타입 객체를 objectT에 할당하기
+    ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
+
+    Type type = genericSuperclass.getActualTypeArguments()[0];
+
+    if(type instanceof ParameterizedType) {
+      this.classT = (Class) ((ParameterizedType) type).getRawType();
+    } else {
+      this.classT = (Class) type;
+    }
+
+    try {
+      this.objectT = classT.getDeclaredConstructor().newInstance();
+    } catch(InstantiationException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    } catch (NoSuchMethodException | InvocationTargetException e) {
+      e.printStackTrace();
+    }
   }
 }
